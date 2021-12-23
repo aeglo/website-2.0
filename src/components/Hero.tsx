d9ed2d1dc0245f59ac277d51bb6cbd18806cf042
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2021-12-23 12:39:38 -0500

    Change hero picture size

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { AspectRatio, Box, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import { AspectRatio, Box, Center, Heading, Image, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 import { MobileMediaOverlay } from "./medias/MobileMediaOverlay";
 
@@ -9,20 +9,18 @@ interface HeroProps {
 
 export const Hero = ({ title, subtitle }: HeroProps) => {
   return (
-    <>
-      <Box id="home" justify="start" paddingTop={{ base: 0, md: "10vh" }}>
-        <Stack direction="column" paddingX={{ base: "5vw", md: "25%" }} textAlign="center">
-          <Heading fontSize={{ base: "4xl", md: "6xl" }}>{title}</Heading>
-          <Text marginTop="2vh" fontSize={{ base: "lg", md: "xl" }} fontWeight="semibold" textAlign="center" marginX={100}>
-            {subtitle}
-          </Text>
-          <MobileMediaOverlay />
-        </Stack>
-        <AspectRatio width="90vw" ratio={16 / 9}>
-          <Image src="images/background_home.png" alt="Home image" />
-        </AspectRatio>
-      </Box>
-    </>
+    <Box id="home" paddingTop={{ base: 0, md: "10vh" }}>
+      <Stack direction="column" paddingX={{ base: "5vw", md: "25%" }} textAlign="center">
+        <Heading fontSize={{ base: "4xl", md: "6xl" }}>{title}</Heading>
+        <Text marginTop="2vh" fontSize={{ base: "lg", md: "xl" }} fontWeight="semibold" textAlign="center" marginX={100}>
+          {subtitle}
+        </Text>
+        <MobileMediaOverlay />
+      </Stack>
+
+      <AspectRatio width="90vw" ratio={16 / 8}>
+        <Image src="images/background_home.png" alt="Home image" />
+      </AspectRatio>
+    </Box>
   );
-  // TODO Add photoshopped picture
 };
