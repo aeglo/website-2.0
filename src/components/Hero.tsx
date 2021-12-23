3e477683e3a72eee70e2b6a9839fd1a20b190923
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2021-12-23 12:26:12 -0500

    Make image bigger

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import { AspectRatio, Box, Heading, Image, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 import { MobileMediaOverlay } from "./medias/MobileMediaOverlay";
 
@@ -10,15 +10,17 @@ interface HeroProps {
 export const Hero = ({ title, subtitle }: HeroProps) => {
   return (
     <>
-      <Box id="home" justify="start" paddingX={{ base: "5vw", md: "25%" }} paddingTop={{ base: 0, md: "10vh" }}>
-        <Stack direction="column" textAlign="center">
+      <Box id="home" justify="start" paddingTop={{ base: 0, md: "10vh" }}>
+        <Stack direction="column" paddingX={{ base: "5vw", md: "25%" }} textAlign="center">
           <Heading fontSize={{ base: "4xl", md: "6xl" }}>{title}</Heading>
           <Text marginTop="2vh" fontSize={{ base: "lg", md: "xl" }} fontWeight="semibold" textAlign="center" marginX={100}>
             {subtitle}
           </Text>
           <MobileMediaOverlay />
-          <Image src="images/background_home.png" alt="Home image" />
         </Stack>
+        <AspectRatio width="90vw" ratio={16 / 9}>
+          <Image src="images/background_home.png" alt="Home image" />
+        </AspectRatio>
       </Box>
     </>
   );
