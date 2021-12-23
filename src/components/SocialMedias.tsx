35ab92edc55a7a873990f07bc851359895fbc65a
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2021-12-23 11:01:34 -0500

    Add fb, github, linkedin and discord logos

--- src/components/SocialMedias.tsx
+++ src/components/SocialMedias.tsx
@@ -1,5 +1,5 @@
 import { Link, Stack } from "@chakra-ui/layout";
-import { Box } from "@chakra-ui/react";
+import { Box, IconButton } from "@chakra-ui/react";
 import React from "react";
 import { DiscordIcon } from "./icons/DiscordIcon";
 import { FacebookIcon } from "./icons/FacebookIcon";
@@ -9,36 +9,49 @@ import { LinkedInIcon } from "./icons/LinkedInIcon";
 interface SocialMediasProps {}
 
 interface SocialMedia {
+  name: string;
   href: string;
-  icon: React.ReactNode;
+  icon: any;
 }
 
 export const SocialMedias: React.FC<SocialMediasProps> = ({}) => {
   const SOCIAL_MEDIAS: Array<SocialMedia> = [
     {
+      name: "Discord",
       href: "https://discord.gg/3VenuWm",
-      icon: <DiscordIcon />
+      icon: <DiscordIcon boxSize={8} />
     },
     {
+      name: "Facebook",
       href: "https://www.facebook.com/AEGLO-101893238568874/",
-      icon: <FacebookIcon />
+      icon: <FacebookIcon boxSize={8} />
     },
     {
+      name: "LinkedIn",
       href: "https://www.linkedin.com/company/19022634",
-      icon: <LinkedInIcon />
+      icon: <LinkedInIcon boxSize={8} />
     },
     {
+      name: "Github",
       href: "https://github.com/aeglo",
-      icon: <GithubIcon />
+      icon: <GithubIcon boxSize={12} />
     }
   ];
 
   return (
     <Box sx={{ position: "sticky", top: "25%" }} marginLeft="80%" marginRight="32px">
-      <Stack direction="column" marginLeft="auto" marginRight="16px">
+      <Stack direction="column" marginLeft="auto" marginRight="16px" spacing={4}>
         {SOCIAL_MEDIAS.map((media) => (
-          <Link key={media.href} href={media.href} isExternal>
-            {media.icon}
+          <Link key={media.name} href={media.href} isExternal>
+            <IconButton
+              aria-label={media.name}
+              icon={media.icon}
+              borderRadius="50%"
+              padding={6}
+              boxSize={16}
+              backgroundColor="black"
+              _hover={{ backgroundColor: "secondary" }}
+            />
           </Link>
         ))}
       </Stack>
--- src/components/icons/FacebookIcon.tsx
+++ src/components/icons/FacebookIcon.tsx
@@ -1,7 +1,18 @@
+import { ChakraProps, Icon } from "@chakra-ui/react";
 import React from "react";
 
-interface FacebookIconProps {}
+interface FacebookIconProps extends Omit<React.SVGAttributes<SVGElement>, keyof ChakraProps>, ChakraProps {}
 
-export const FacebookIcon: React.FC<FacebookIconProps> = ({}) => {
-  return <div>facebook</div>;
-};
+export const FacebookIcon: React.FC<FacebookIconProps> = (props) => (
+  <Icon viewBox="0 0 96 96" {...props}>
+    <svg>
+      <path
+        d="M72.089,0.02L59.624,0C45.62,0,36.57,9.285,36.57,23.656v10.907H24.037c-1.083,0-1.96,0.878-1.96,1.961v15.803
+		c0,1.083,0.878,1.96,1.96,1.96h12.533v39.876c0,1.083,0.877,1.96,1.96,1.96h16.352c1.083,0,1.96-0.878,1.96-1.96V54.287h14.654
+		c1.083,0,1.96-0.877,1.96-1.96l0.006-15.803c0-0.52-0.207-1.018-0.574-1.386c-0.367-0.368-0.867-0.575-1.387-0.575H56.842v-9.246
+		c0-4.444,1.059-6.7,6.848-6.7l8.397-0.003c1.082,0,1.959-0.878,1.959-1.96V1.98C74.046,0.899,73.17,0.022,72.089,0.02z"
+        fill="#FFF"
+      />
+    </svg>
+  </Icon>
+);
