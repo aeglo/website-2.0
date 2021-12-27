ec1e7aefdf7311779a19b20a1e247cc2da13b955
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2021-12-27 16:45:47 -0500

    Make email address in footer secondary color

--- src/components/footer/mobile/MobileCoordinates.tsx
+++ src/components/footer/mobile/MobileCoordinates.tsx
@@ -3,25 +3,35 @@ import { useI18n } from "next-rosetta";
 import { AegloLocale } from "../../../i18n";
 
 export const MobileCoordinates = () => {
-    const i18n = useI18n<AegloLocale>();
-    const { t } = i18n;
+  const i18n = useI18n<AegloLocale>();
+  const { t } = i18n;
 
-    return (
-        <Box>
-            <VStack justify="center">
-                <AspectRatio width="128px">
-                    <Img src="images/aeglo_logo.png" fallbackSrc="images/aeglo_logo.png" alt="Aeglo logo" />
-                </AspectRatio>
-                <Stack direction="column" spacing={0}>
-                    <Text fontSize="20px" fontWeight="bold" color="white" align="center">AEGLO - Université Laval</Text>
-                    <Text fontSize="16px" fontWeight="normal" color="white" align="center">{t("footer.address")},</Text>
-                    <Text fontSize="16px" fontWeight="normal" color="white" align="center">Université Laval, G1V 0A6</Text>
-                    <Stack direction="row" justify="center">
-                        <Text fontSize="16px" fontWeight="bold" color="white" align="center">{t("footer.email")}: </Text>
-                        <Link fontSize="16px" fontWeight="normal" color="white" href="mailto:aeglo@aeglo.ift.ulaval.ca" align="center">aeglo@aeglo.ift.ulaval.ca</Link>
-                    </Stack>
-                </Stack>
-            </VStack>
-        </Box>
-    );
+  return (
+    <Box>
+      <VStack justify="center">
+        <AspectRatio width="128px">
+          <Img src="images/aeglo_logo.png" fallbackSrc="images/aeglo_logo.png" alt="Aeglo logo" />
+        </AspectRatio>
+        <Stack direction="column" spacing={0}>
+          <Text fontSize="20px" fontWeight="bold" color="white" align="center">
+            AEGLO - Université Laval
+          </Text>
+          <Text fontSize="16px" fontWeight="normal" color="white" align="center">
+            {t("footer.address")},
+          </Text>
+          <Text fontSize="16px" fontWeight="normal" color="white" align="center">
+            Université Laval, G1V 0A6
+          </Text>
+          <Stack direction="row" justify="center">
+            <Text fontSize="16px" fontWeight="bold" color="white" align="center">
+              {t("footer.email")}:{" "}
+            </Text>
+            <Link fontSize="16px" fontWeight="normal" color="secondary.default" href="mailto:aeglo@aeglo.ift.ulaval.ca" align="center">
+              aeglo@aeglo.ift.ulaval.ca
+            </Link>
+          </Stack>
+        </Stack>
+      </VStack>
+    </Box>
+  );
 };
