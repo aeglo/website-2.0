61049e6583627e51dde89a139f52bb804aeaa71f
Christophe Duchesne <cduchesne.1@gmail.com>
2022-08-14 14:43:26 -0400

    fix: adapt sponsors page for mobile

--- src/components/partners/PartnerItem.tsx
+++ src/components/partners/PartnerItem.tsx
@@ -7,15 +7,17 @@ import { WebIcon } from "../icons/WebIcon";
 interface PartnerItemProps {
     partner: Partner;
     backgroundColor: string;
+    maxHeight: string;
     itemKey: any;
 }
 
-export const PartnerItem = ({ partner, itemKey, backgroundColor }: PartnerItemProps) => {
+export const PartnerItem = ({ partner, itemKey, backgroundColor, maxHeight }: PartnerItemProps) => {
     const [showInfos, setShowInfos] = useState(false);
 
     return (
         <Flex
             flex={1}
+            maxHeight={{ base: maxHeight, md: '100%' }}
             backgroundImage={`url(${partner.asset})`}
             backgroundColor={backgroundColor}
             backgroundSize="contain"
@@ -24,14 +26,14 @@ export const PartnerItem = ({ partner, itemKey, backgroundColor }: PartnerItemPr
             onMouseEnter={() => setShowInfos(true)}
             onMouseLeave={() => setShowInfos(false)} >
             {showInfos && (<Box display="flex" flexDirection="column" w='100%' h='100%' bgGradient='linear(to-t, darkSection, rgba(27, 31, 43, 0.42))' alignItems="center" justifyContent="flex-end">
-                <Text marginTop="2vh" fontSize={{ base: "16px", md: "20px" }} fontWeight="bold" textAlign="center" marginX={100} color="white">
+                <Text marginTop="2vh" fontSize={{ base: "16px", md: "20px" }} fontWeight="bold" textAlign="center" marginX={50} color="white" overflowY="scroll">
                     {partner.descriptionFR}
                 </Text>
                 <Stack direction={"row"} mt="48px" display="flex" justifyContent="center">
-                    <Link key={itemKey} href={partner.website} isExternal>
+                    <Link key={'web' + itemKey} href={partner.website} isExternal>
                         <WebIcon w="28" h="28" />
                     </Link>
-                    <Link key={itemKey} href={partner.linkedin} isExternal>
+                    <Link key={'linkedin' + itemKey} href={partner.linkedin} isExternal>
                         <LinkedInIcon boxSize={6} color="white" />
                     </Link>
                 </Stack>
