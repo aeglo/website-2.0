731b1f32201a7100daf8eefdc8546607b483cbff
Christophe Duchesne <cduchesne.1@gmail.com>
2022-08-22 15:23:18 -0400

    fix: problems on pedagogy page

--- src/components/pedagogy/ProblemSolvingSection.tsx
+++ src/components/pedagogy/ProblemSolvingSection.tsx
@@ -1,5 +1,5 @@
-import { Box, Heading, Text } from "@chakra-ui/layout";
-import { Center } from "@chakra-ui/react";
+import { Heading, Text } from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/react";
 import React from "react";
 
 interface ProblemSolvingSectionProps {
@@ -9,15 +9,13 @@ interface ProblemSolvingSectionProps {
 
 export const ProblemSolvingSection: React.FC<ProblemSolvingSectionProps> = ({ title, subtitle }) => {
   return (
-    <Center marginTop={16} background="main">
-      <Box height="789px" paddingTop={"128px"} textAlign="center" paddingX="20vw">
-        <Heading fontSize="52px" color="white">
-          {title}
-        </Heading>
-        <Text fontSize="24px" color="white" fontWeight={"bold"} marginTop={"48px"}>
-          {subtitle}
-        </Text>
-      </Box>
-    </Center>
+    <Flex height="789px" marginTop={16} paddingTop={"128px"} textAlign="center" paddingX={"20vw"} flexDir={"column"} backgroundColor={"main"}>
+      <Heading fontSize={{ base: "36px", md: "52px" }} color="white">
+        {title}
+      </Heading>
+      <Text style={{ whiteSpace: 'pre-line'}} fontSize={{ base: "16px", md: "24px" }} color="white" fontWeight={"bold"} marginTop={"48px"} overflow={"none"}>
+        {subtitle}
+      </Text>
+    </Flex>
   );
 };
