3ad43a5ac6f1130f2ab994300de2e89859d1f6f3
Christophe Duchesne <58085585+cduchesne1@users.noreply.github.com>
2022-08-26 15:52:06 -0400

    feat: hide side socials when scrolling (#59)

--- src/components/medias/DesktopMediaOverlay.tsx
+++ src/components/medias/DesktopMediaOverlay.tsx
@@ -1,15 +1,36 @@
 import { Box } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SocialMedias } from "./SocialMedias";
 
-interface DesktopMediaOverlayProps {}
+interface DesktopMediaOverlayProps { }
 
-export const DesktopMediaOverlay: React.FC<DesktopMediaOverlayProps> = ({}) => {
+export const DesktopMediaOverlay: React.FC<DesktopMediaOverlayProps> = ({ }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const listenToScroll = () => {
+    let heightToHideFrom = 1000;
+    const winScroll = document.body.scrollTop ||
+      document.documentElement.scrollTop;
+
+    if (winScroll > heightToHideFrom) {
+      isVisible &&
+        setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () =>
+      window.removeEventListener("scroll", listenToScroll);
+  }, []);
+
   return (
     <Box position="absolute" height="100%" right="10%" zIndex={2}>
-      <Box sx={{ position: "sticky", top: "25%" }} display={{ base: "none", md: "grid" }}>
-        <SocialMedias direction="column" color="main"/>
-      </Box>
+      {isVisible && (<Box sx={{ position: "sticky", top: "25%" }} display={{ base: "none", md: "grid" }}>
+        <SocialMedias direction="column" color="main" />
+      </Box>)}
     </Box>
   );
 };
