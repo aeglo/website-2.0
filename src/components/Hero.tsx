c8a085e78a67bc44071070b7a2ac60588b4163dc
Christophe Duchesne <58085585+cduchesne1@users.noreply.github.com>
2023-01-06 16:31:51 -0500

    feat: create reusable hero component (#74)

---
+++ src/components/Hero.tsx
@@ -0,0 +1,42 @@
+import {
+  Box, Fade, Flex, Heading, Text,
+} from '@chakra-ui/react';
+import React from 'react';
+
+interface HeroProps {
+  img: string;
+  title: string;
+  subtitle: string;
+  children?: React.ReactNode;
+}
+
+export default function Hero({
+  img, title, subtitle, children,
+}: HeroProps) {
+  return (
+    <Box
+      w="100%"
+      h="100vh"
+      bgImg={`linear-gradient(rgba(27, 31, 43, 0.79), rgba(27, 31, 43, 0.79)), url(${img})`}
+      bgRepeat="no-repeat"
+      bgPosition="center"
+      bgSize="cover"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      px={{ base: '1.5rem', lg: '8rem' }}
+    >
+      <Fade in>
+        <Heading fontSize={{ base: '3.125rem', lg: '12.25rem' }} color="white" textAlign={{ base: 'center', lg: 'start' }}>
+          {title}
+        </Heading>
+      </Fade>
+      <Fade in>
+        <Text fontSize={{ base: '1.25rem', lg: '1.5rem' }} fontWeight="bold" color="white" whiteSpace="pre-line" textAlign={{ base: 'center', lg: 'start' }}>{subtitle}</Text>
+      </Fade>
+      <Fade in>
+        <Flex justify={{ base: 'center', lg: 'start' }} mt="1rem">{children}</Flex>
+      </Fade>
+    </Box>
+  );
+}
