513645ae719a1c6bdc32b65c1d1a096fec5606d5
Christophe Duchesne <58085585+cduchesne1@users.noreply.github.com>
2023-01-06 10:54:53 -0500

    chore: setup airbnb eslint (#70)

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,30 +1,30 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react';
 
 const fonts = {
-    heading: `'Outfit', sans-serif`,
-    body: `'Inter', sans-serif`,
-}
+  heading: '\'Outfit\', sans-serif',
+  body: '\'Inter\', sans-serif',
+};
 
 const breakpoints = {
   sm: '40em',
   md: '52em',
   lg: '64em',
   xl: '80em',
-}
+};
 
 const theme = extendTheme({
-    colors: {
-        white: "#fff",
-        black: "#16161D",
-        main: "#242A2D",
-        secondary: {
-            default: "#0577FF",
-            hover: "#6baeff"
-        },
-        darkSection: "#1B1F2B"
+  colors: {
+    white: '#fff',
+    black: '#16161D',
+    main: '#242A2D',
+    secondary: {
+      default: '#0577FF',
+      hover: '#6baeff',
     },
-    fonts,
-    breakpoints,
-})
+    darkSection: '#1B1F2B',
+  },
+  fonts,
+  breakpoints,
+});
 
-export default theme
+export default theme;
