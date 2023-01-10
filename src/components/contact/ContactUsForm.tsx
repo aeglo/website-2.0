3bcf6855661f59c2ad823cca56733ef34bc8512a
Christophe Duchesne <cduchesne.1@gmail.com>
2023-01-10 12:50:32 -0500

    fix: some merge problems

--- src/components/contact/ContactUsForm.tsx
+++ src/components/contact/ContactUsForm.tsx
@@ -2,74 +2,22 @@ import { CheckIcon } from '@chakra-ui/icons';
 import {
   Button, FormLabel, Input, Spinner, Textarea, VStack, Text,
 } from '@chakra-ui/react';
-<<<<<<< Updated upstream
-import React, { useEffect, useState } from 'react';
-import useSendContactUsEmail from '../../hooks/useSendContactUsEmail';
-import isStringEmptyOrNull from '../../utils/isStringEmptyOrNull';
-=======
 import { useForm, ValidationError } from '@formspree/react';
 import React from 'react';
->>>>>>> Stashed changes
 
 interface ContactUsFormProps {
-  publicKey: string;
-  serviceId: string;
-  templateId: string;
+  formId: string;
 }
 
-<<<<<<< Updated upstream
-export default function ContactUsForm({ publicKey, serviceId, templateId }: ContactUsFormProps) {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [body, setBody] = useState('');
-  const [isNameInvalid, setIsNameInvalid] = useState(false);
-  const [isEmailInvalid, setIsEmailInvalid] = useState(false);
-  const [isBodyInvalid, setIsBodyInvalid] = useState(false);
-
-  const { status, sendEmail } = useSendContactUsEmail({ publicKey, serviceId, templateId });
-
-  useEffect(() => {
-    setIsNameInvalid(false);
-  }, [name]);
-
-  useEffect(() => {
-    setIsEmailInvalid(false);
-  }, [email]);
-
-  useEffect(() => {
-    setIsBodyInvalid(false);
-  }, [body]);
-
-  useEffect(() => {
-    if (status === 'success') {
-      setName('');
-      setEmail('');
-      setBody('');
-    }
-  }, [status]);
-
-  const handleFormSubmit = () => {
-    setIsNameInvalid(isStringEmptyOrNull(name));
-    setIsEmailInvalid(isStringEmptyOrNull(email));
-    setIsBodyInvalid(isStringEmptyOrNull(body));
-
-    // TODO clean up ca
-    if (!(isStringEmptyOrNull(name) || isStringEmptyOrNull(email) || isStringEmptyOrNull(body))) {
-      sendEmail(name, email, body);
-    }
-  };
-
-=======
 export default function ContactUsForm({ formId }: ContactUsFormProps) {
   const [state, handleSubmit] = useForm(formId);
 
->>>>>>> Stashed changes
   const getButtonIcon = () => {
-    if (status === 'loading') {
+    if (state.submitting) {
       return <Spinner />;
     }
 
-    if (status === 'success') {
+    if (state.succeeded) {
       return <CheckIcon />;
     }
 
@@ -78,30 +26,6 @@ export default function ContactUsForm({ formId }: ContactUsFormProps) {
 
   return (
     <VStack width={{ base: '100%', lg: '35%' }} alignContent={{ base: 'center', lg: 'start' }}>
-<<<<<<< Updated upstream
-      <FormControl isRequired isInvalid={isNameInvalid}>
-        <FormLabel>Nom complet</FormLabel>
-        <Input value={name} onChange={({ target }) => setName(target.value)} />
-      </FormControl>
-      <FormControl isRequired isInvalid={isEmailInvalid}>
-        <FormLabel>Adresse courriel</FormLabel>
-        <Input value={email} onChange={({ target }) => setEmail(target.value)} />
-      </FormControl>
-      <FormControl isRequired isInvalid={isBodyInvalid}>
-        <FormLabel>Message</FormLabel>
-        <Textarea value={body} onChange={({ target }) => setBody(target.value)} resize="none" minHeight={124} />
-      </FormControl>
-      {status === 'error' && <Text color="red">Une erreur est survenue lors de l&apos;envoie du courriel. SVP réessayer ou nous contacter directement à aeglo@aeglo.ift.ulaval.ca</Text>}
-      <Button
-        onClick={() => handleFormSubmit()}
-        backgroundColor="secondary.default"
-        color="white"
-        _hover={{ backgroundColor: 'secondary.hover' }}
-        rightIcon={getButtonIcon()}
-      >
-        Envoyer
-      </Button>
-=======
       <form onSubmit={handleSubmit} method="POST">
         <FormLabel>Adresse courriel</FormLabel>
         <Input name="email" id="email" isRequired />
@@ -122,7 +46,6 @@ export default function ContactUsForm({ formId }: ContactUsFormProps) {
           Envoyer
         </Button>
       </form>
->>>>>>> Stashed changes
     </VStack>
   );
 }
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -9,24 +9,18 @@ import Layout from '../layout/Layout';
 export async function getStaticProps() {
   return {
     props: {
-      publicKey: process.env.EMAIL_PUBLIC_KEY,
-      serviceId: process.env.EMAIL_SERVICE,
-      templateId: process.env.EMAIL_TEMPLATE,
+      formId: process.env.FORM_ID,
       mapsApiKey: process.env.MAPS_API_KEY,
     },
   };
 }
 
 interface ContactProps {
-  publicKey: string;
-  serviceId: string;
-  templateId: string;
+  formId: string;
   mapsApiKey: string;
 }
 
-export default function Contact({
-  publicKey, serviceId, templateId, mapsApiKey,
-}: ContactProps) {
+export default function Contact({ formId, mapsApiKey }: ContactProps) {
   return (
     <>
       <Head>
@@ -78,11 +72,7 @@ export default function Contact({
             direction={{ base: 'column', lg: 'row' }}
             justify="space-between"
           >
-            <ContactUsForm
-              publicKey={publicKey}
-              serviceId={serviceId}
-              templateId={templateId}
-            />
+            <ContactUsForm formId={formId} />
             <GoogleMap mapsApiKey={mapsApiKey} />
           </Flex>
         </Flex>
