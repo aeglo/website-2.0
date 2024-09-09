1e5827c5fa0155f019f186c7423048323e803f73
Xavier Hamel <xavierhamel02+git@gmail.com>
2024-09-09 15:26:16 -0400

    fix linting from vercel

--- src/data/events.ts
+++ src/data/events.ts
@@ -14,16 +14,15 @@ interface Event {
 const events: Event[] = [
   {
     name: "Épluchette de blé d'Inde",
-    date:{
+    date: {
       day: 21,
       month: 9,
-      year: 2024
+      year: 2024,
     },
-    time:"16h30",
-    location:"Base de plein air de Sainte-Foy",
-    link:"https://evelya.co/universite-laval/genie-logiciel/epluchette-de-bledinde-de-l-aeglo-4372?fbclid=IwY2xjawFMHF5leHRuA2FlbQIxMQABHRAxNNZ2dR3olBaQbyQ3cTWuo7kXkb-QOFPBmTm8cNnHv0_JRW3PWkDczw_aem_oQugMXeATMtl36uJCjUzAw"
-  }
-
+    time: "16h30",
+    location: "Base de plein air de Sainte-Foy",
+    link: "https://evelya.co/universite-laval/genie-logiciel/epluchette-de-bledinde-de-l-aeglo-4372?fbclid=IwY2xjawFMHF5leHRuA2FlbQIxMQABHRAxNNZ2dR3olBaQbyQ3cTWuo7kXkb-QOFPBmTm8cNnHv0_JRW3PWkDczw_aem_oQugMXeATMtl36uJCjUzAw",
+  },
 ];
 
 export default events;
