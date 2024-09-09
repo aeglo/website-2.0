95f3992cc4fa7785451974e838167271583137a9
Xavier Hamel <xavierhamel02+git@gmail.com>
2024-09-09 15:34:43 -0400

    fix 2

--- src/data/events.ts
+++ src/data/events.ts
@@ -19,9 +19,9 @@ const events: Event[] = [
       month: 9,
       year: 2024,
     },
-    time: "16h30",
-    location: "Base de plein air de Sainte-Foy",
-    link: "https://evelya.co/universite-laval/genie-logiciel/epluchette-de-bledinde-de-l-aeglo-4372?fbclid=IwY2xjawFMHF5leHRuA2FlbQIxMQABHRAxNNZ2dR3olBaQbyQ3cTWuo7kXkb-QOFPBmTm8cNnHv0_JRW3PWkDczw_aem_oQugMXeATMtl36uJCjUzAw",
+    time: '16h30',
+    location: 'Base de plein air de Sainte-Foy',
+    link: 'https://evelya.co/universite-laval/genie-logiciel/epluchette-de-bledinde-de-l-aeglo-4372?fbclid=IwY2xjawFMHF5leHRuA2FlbQIxMQABHRAxNNZ2dR3olBaQbyQ3cTWuo7kXkb-QOFPBmTm8cNnHv0_JRW3PWkDczw_aem_oQugMXeATMtl36uJCjUzAw',
   },
 ];
 
